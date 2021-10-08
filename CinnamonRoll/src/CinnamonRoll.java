0dcc3e18876802632479267406decca7d1ca2d65
ChristieZS <christiexx@gmail.com>
2021-10-08 11:52:55 +0100

    Added file scanner

--- CinnamonRoll/src/CinnamonRoll.java
+++ CinnamonRoll/src/CinnamonRoll.java
@@ -1,18 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CinnamonRoll {
 
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) throws FileNotFoundException {	
 
-		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
+		Scanner in = new Scanner(new File("src/pigs.txt"));
+
+		List<String> pigs = new ArrayList<String>();
 		
-		if(s.equals("Marshmallow")) {
-			System.out.println("Nom Nom Marshmallow");
+		while(in.hasNextLine()) {
+			pigs.add(in.nextLine());
 		}
-		else {
-			System.out.println("Who you?");
+		
+		for(int i = 0; i < pigs.size(); i++ ) {
+			System.out.println("name: " + pigs.get(i));
 		}
 		
 		in.close();
